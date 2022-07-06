399d6f64081d836c72d9f6078daec435fd112d89
Luiz Vieira <luiz22_sp@hotmail.com>
2022-07-06 08:55:12 -0300

    Frameworks checkebox

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { EstadoBr } from '../shared/models/estado-br';
 import { ConsultaCepService } from '../shared/services/consulta-cep.service';
@@ -14,7 +14,11 @@ import { DropdownService } from '../shared/services/dropdown.service';
 export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
-  estados$: Observable<EstadoBr>;
+  estados$: Observable<EstadoBr[]>;
+  cargos: any[];
+  tecnologias: any[];
+  newsletter: any[];
+  frameworks = ['Angular', 'React', 'Vue', 'Sencha'];
 
   constructor(
     private fb: FormBuilder,
@@ -35,15 +39,36 @@ export class DataFormComponent implements OnInit {
         bairro: [null, Validators.required],
         cidade: [null, Validators.required],
         estado: [null, Validators.required]
-      })
+      }),
+      cargo: [null],
+      tecnologias: [null],
+      newsletter: ['sim'],
+      termos: [null, [Validators.pattern('true'), Validators.required]],
+      frameworks: this.buildFrameworks()
 		});
 
     this.estados$ = this.dropdownService.getEstadoBr();
+    this.cargos = this.dropdownService.getCargos();
+    this.tecnologias = this.dropdownService.getTecnologias();
+    this.newsletter = this.dropdownService.getNewletter();
+  }
+
+  buildFrameworks(){
+    const values = this.frameworks.map((v) => new FormControl(false));
+    return this.fb.array(values);
   }
 
   onSubmit() {
+    let valueSubmit = Object.assign({}, this.formulario.value);
+    valueSubmit = Object.assign(valueSubmit, {
+      frameworks: valueSubmit.frameworks
+        .map((v, i) => v ? this.frameworks[i] : null)
+        .filter(v => v !== null)
+    });
+    console.log(valueSubmit);
+
     if(this.formulario.valid){
-      this.http.post('https://httpbin.org/post', this.formulario.value)
+      this.http.post('https://httpbin.org/post', valueSubmit)
       .subscribe(dados => {
           console.log(dados['json']);
           this.resetarForm();
@@ -112,4 +137,18 @@ export class DataFormComponent implements OnInit {
       'is-invalid': this.verificaValidTouched(campo)
     }
   }
+
+  compareWith(obj1, obj2) {
+    return obj1 && obj2 && obj1.desc === obj2.desc;
+  }
+
+  setarCargo(){
+    const cargo = {nome: 'Dev', nivel: 'Pleno', desc: 'Dev Pleno'};
+    this.formulario.get('cargo').setValue(cargo);
+  }
+
+  setarTecnologia(){
+    this.formulario.get('tecnologias').setValue(['java', 'javascript', 'ruby']);
+  }
+
 }
--- src/app/shared/services/dropdown.service.ts
+++ src/app/shared/services/dropdown.service.ts
@@ -9,18 +9,42 @@ import { EstadoBr } from '../models/estado-br';
 })
 export class DropdownService {
 
-constructor(
-  private http: HttpClient
-) { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-getEstadoBr(): Observable<EstadoBr>{
-  return this.http.get<EstadoBr>('assets/dados/estadosbr.json').pipe(
-    map(dados => dados),
-    catchError(error => {
-      console.log('ERRO NA REQUISIÇÃO:',error);
-      return throwError(error);
-    })
-  )
-}
+  getEstadoBr(): Observable<EstadoBr[]> {
+    return this.http.get<EstadoBr[]>('assets/dados/estadosbr.json').pipe(
+      map(dados => dados),
+      catchError(error => {
+        console.log('ERRO NA REQUISIÇÃO:', error);
+        return throwError(error);
+      })
+    )
+  }
+
+  getCargos(){
+    return [
+      {nome: 'Dev', nivel: 'Junior', desc: 'Dev Jr'},
+      {nome: 'Dev', nivel: 'Pleno', desc: 'Dev Pleno'},
+      {nome: 'Dev', nivel: 'Sênior', desc: 'Dev Sr'}
+    ];
+  }
+
+  getTecnologias(){
+    return [
+      {nome: 'java', desc: 'Java'},
+      {nome: 'javascript', desc: 'Javascript'},
+      {nome: 'php', desc: 'PHP'},
+      {nome: 'ruby', desc: 'Ruby'}
+    ];
+  }
+
+  getNewletter(){
+    return [
+      {valor: 'sim', desc: 'Sim'},
+      {valor: 'nao', desc: 'Não'}
+    ];
+  }
 
 }
