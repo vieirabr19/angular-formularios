f01bf4d054a4ec19515e378ad5dfc6e4d952ce00
Luiz Vieira <luiz22_sp@hotmail.com>
2022-07-23 21:18:26 -0300

    Filtrando as cidades por ID do estado

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -63,7 +63,6 @@ export class DataFormComponent extends FormBaseComponent implements OnInit {
     // this.estados$ = this.dropdownService.getEstadoBr();
     this.dropdownService.getEstadoBr().subscribe(estados => this.estados = estados);
     this.dropdownService.getCidadeBr().subscribe(cidades => this.cidades = cidades);
-    this.dropdownService.getCidadeBrId(5).subscribe();
     this.cargos = this.dropdownService.getCargos();
     this.tecnologias = this.dropdownService.getTecnologias();
     this.newsletter = this.dropdownService.getNewletter();
@@ -100,8 +99,8 @@ export class DataFormComponent extends FormBaseComponent implements OnInit {
 
     this.formulario.get('endereco.estado').valueChanges
       .pipe(
-        // tap(estado => console.log('Novo estado', estado)),
-        map(estados => this.estados.filter(estado => estado.sigla == estados )),
+        tap(estado => console.log( 'Novo estado',estado)),
+        map(estados => this.estados.filter(cidade => cidade.sigla == estados)),
         map(estado => estado && estado.length > 0 ? estado[0].id : EMPTY),
         switchMap(idEstado => this.dropdownService.getCidadeBrId(Number(idEstado)))
       )
